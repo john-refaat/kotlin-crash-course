1bd60c1911870af162aaabf9f3209c734461d68b
John Stefanos <john.refaat88@gmail.com>
2025-07-27 08:09:56 +0300

    list, mutable list

--- src/main/kotlin/fromjava.kt
+++ src/main/kotlin/fromjava.kt
@@ -14,8 +14,18 @@ enum class Color {
 }
 
 fun main(args: Array<String>) {
+    println(args)
     updateWeather(5)
     updateWeather(10)
     updateWeather(25)
     updateWeather(30)
+    var languages = mutableListOf("Java", "Python")
+    languages.add("Kotlin")
+    var langs = listOf("Java", "Python")
+    //langs.add("Kotlin")
+    var ls = arrayOf("Java", "Python")
+    ls[1] = "Kotlin"
+    val lss = ls.plus("Scala")
+    ls.forEach { println(it) }
+    println(lss.toList().toString())
 }
