fc173ae1345387377be5a26a99dffa04df7437b6
John Stefanos <john.refaat88@gmail.com>
2025-08-21 06:45:29 +0300

    Types

---
+++ src/main/java/model/Session.java
@@ -0,0 +1,14 @@
+package model;
+
+import org.jetbrains.annotations.Nullable;
+
+public class Session {
+    public String getDescription() {
+        return null;
+    }
+
+    @Nullable
+    public String getName() {
+        return null;
+    }
+}
